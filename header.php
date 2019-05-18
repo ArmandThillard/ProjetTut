5f6e187533aec175edfa907eadba15e7e2dd3763
Bourdow <hugohbh@gmail.com>
2019-05-18 22:56:04 +0200

    JLE BZ J'AI DIT

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <header>
 
     <!-- Call for Bootstrap, JQuery-->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="./style/searchbar.css">
 
--- index.php
+++ index.php
@@ -7,6 +7,36 @@
 </div>
 <body>
 
+<!-- Carousel Catégories -->
+<link rel="stylesheet" type="text/css" href="./style/carousel.css">
+<div id="carousel-categories" class="carousel slide" data-ride="carousel">
+    <ul class="carousel-indicators">
+        <li data-target="#carousel-categories" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-categories" data-slide-to="1"></li>
+        <li data-target="#carousel-categories" data-slide-to="2"></li>
+    </ul>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="./img/categories/cat-animaux.jpg" alt="catégorie animaux">
+            <div class="carousel-caption">
+                <h3>Animaux</h3>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/categories/cat-nature.jpg" alt="catégorie nature">
+            <div class="carousel-caption">
+                <h3>Nature</h3>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/categories/cat-sport.jpg" alt="catégorie sport">
+            <div class="carousel-caption">
+                <h3>Sport</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 <?php
     require("./footer.php");
