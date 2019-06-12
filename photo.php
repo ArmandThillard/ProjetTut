62a79b0226fc2cc20614d98329e9f3f600172a3f
Bourdow <hugohbh@gmail.com>
2019-06-12 23:39:58 +0200

    Je remet les margins qui avaient disparu ducoup

--- photo.php
+++ photo.php
@@ -57,7 +57,7 @@
 
     <div class="row">
         <div class="col-6" style="height : 50vh;">
-			<img class='img-fluid ' src=<?php echo $donneesImages[10]; ?> style=' height : 100%;'>
+			<img class='img-fluid img-detail' src=<?php echo $donneesImages[10]; ?> style=' height : 100%;'>
         </div>
         <div class="col-6">
             <div class="container text-left">
