525a63e02283d0b9b8e6014a2dec36d01d3cf267
ArmandKrok <thillard.armand@gmail.com>
2019-06-12 10:34:03 +0200

    C'est armand bonjour je viens de travailler

--- recherche.php
+++ recherche.php
@@ -33,43 +33,52 @@
         <div id="searchedImages" class="col-9">
             <h1 class="font-weight-bold text-light"><small> Résultats de la recherche pour : <?=$_GET['search']?> </small></h1>
             <?php
-                if(!isset($_GET['search'])){
-                    $_GET['search'] = '';
-                }
-                $_GET['search'] = strtolower($_GET['search']);
-                $search = explode(' ',$_GET['search']);
-                $requete = 'SELECT image.id_image id, image.lien_image_fili lien FROM image, tag, referencer, correspondre WHERE image.id_image = referencer.id_image
-                                AND tag.id_tag = referencer.id_tag AND image.id_image = correspondre.id_image AND ';
+                $filters=false;
+                $requete = 'SELECT DISTINCT image.id_image id, image.lien_image_fili lien FROM image, tag, referencer, correspondre, categorie WHERE ';
                 //recherche des mots-clés dans : TAGS, NOM_IMAGE
-                $requete = $requete.'(';
-                for($i = 0; $i < count($search); $i++){
-                    $requete = $requete."(tag.nom_tag LIKE '%".$search[$i]."%' OR image.nom_image LIKE '%".$search[$i]."%')";
-                    if($i<count($search) - 1){
-                        $requete = $requete." OR";
-                    }else{
-                        $requete = $requete.")";
+
+                if(isset($_GET['search']) && !empty($_GET['search'])){
+                    $filters = true;
+                    $_GET['search'] = strtolower($_GET['search']);
+                    $search = explode(' ',$_GET['search']);
+                    $requete = $requete." image.id_image = referencer.id_image AND tag.id_tag = referencer.id_tag AND (";
+                    for($i = 0; $i < count($search); $i++){
+                        $requete = $requete."(tag.nom_tag LIKE '%".$search[$i]."%' OR image.nom_image LIKE '%".$search[$i]."%')";
+                        if($i<count($search) - 1){
+                            $requete = $requete." OR";
+                        }else{
+                            $requete = $requete.")";
+                        }
                     }
                 }
                 // Insertion du prix si renseigné
                 if(isset($_GET['prix-min']) && !empty($_GET['prix-min'])){
+                    $filters = true;
                     $requete = $requete." AND image.prix_ht_image >= ".$_GET['prix-min'];
                 }
                 if(isset($_GET['prix-max']) && !empty($_GET['prix-min'])){
+                    $filters = true;
                     $requete = $requete." AND image.prix_ht_image <= ".$_GET['prix-max'];
                 }
                 // Insertion des catégories si cochées
-                $res = $link->query("SELECT id_categorie FROM categorie");
-                while($data = $res->fetch()){
-                    if(isset($_GET[$data['id_categorie']])){
-                        $requete = $requete." AND correspondre.id_categorie = ".$data['id_categorie'];
+                if(isset($_GET['cat'])){
+                    $filters = true;
+                    $requete = $requete." image.id_image = correspondre.id_image AND correspondre.id_categorie = categorie.id_categorie";
+                    $res = $link->query("SELECT nom_categorie FROM categorie");
+                    while($data = $res->fetch()){
+                        if(isset($_GET['cat']) && $_GET['cat'] == $data['nom_categorie']){
+                            $requete = $requete."  AND categorie.nom_categorie = '".$_GET['cat']."'";
+                        }
                     }
                 }
 
                 // Images visibles uniquement
-                $requete = $requete." AND image.image_visible = 1";
+                if($filters){
+                    $requete = $requete." AND ";
+                }
+                $requete = $requete." image.image_visible = 1";
                 // De la plus récente à la plus ancienne
                 $requete = $requete." ORDER BY image.date_upload_image DESC";
-                echo $requete;
                 $res = $link->query($requete);
                 $nbImages = 0;
                 echo '<div class="row">';
